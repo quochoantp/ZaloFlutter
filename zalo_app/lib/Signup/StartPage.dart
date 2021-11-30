9a96d2eaac82981035e5653b2349058615e37dc4
Long122k <longdo8000@gmail.com>
2021-11-30 23:12:35 +0700

    create start page

---
+++ zalo_app/lib/Signup/StartPage.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:zalo_app/Signup/SignUpPage.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+              ),
+              Text(
+                'Zalo',
+                style: TextStyle(
+                  fontSize: 100,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+              SizedBox(
+                height: 250,
+              ),
+              signInButton(),
+              SizedBox(
+                height: 20,
+              ),
+              signUpButton(),
+              Expanded(child: Container()),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Tieng Viet",
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "English",
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "日本語",
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          )),
+    );
+  }
+
+  Widget signUpButton() {
+    return InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SignUpPage()),
+          );
+        },
+        child: Container(
+          height: 50,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Color(0xFFE0E0E0),
+          ),
+          child: Center(
+            child: Text(
+              'Đăng ký',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ));
+  }
+
+  Widget signInButton() {
+    return InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SignUpPage()),
+          );
+        },
+        child: Container(
+          height: 50,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.blue,
+          ),
+          child: Center(
+            child: Text(
+              'Đăng nhập',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ));
+  }
+}
--- zalo_app/lib/main.dart
+++ zalo_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:zalo_app/Signup/SignUpPage.dart';
 
+import 'Signup/StartPage.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,8 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          primaryColor: Color(0xFF0288D1	), accentColor: Color(0x664FC37F)),
-      home: SignUpPage(),
+          primaryColor: Color(0xFF0288D1), accentColor: Color(0x664FC37F)),
+      home: StartPage(),
     );
   }
 }
